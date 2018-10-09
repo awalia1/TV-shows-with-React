cda053a9b5dd9b29cd04a6c5d8f4383d52bc408d
Amarpreet Walia <amarpreet.s.walia@gmail.com>
2018-10-09 12:56:47 -0400

    added user input

--- src/components/Shows/index.js
+++ src/components/Shows/index.js
@@ -4,19 +4,24 @@ import ShowsList from '../ShowsList'
 class Shows extends Component {
 
     state = {
-        shows: []
-      }
-    
-      componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=avatar-the-last-airbender')
+        shows: [],
+        showName: '',
+        isFetching: false
+    }
+   
+    onShowInputChange = e => {
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
           .then(response => response.json())
           .then(json => this.setState({ shows: json }));
-      }
+    }
 
     render() {
         return (
             <div>
                 The length of shows array - {this.state.shows.length}
+                <div>
+                    <input type="text" onChange={this.onShowInputChange} />
+                </div>
                 <ShowsList list = {this.state.shows} />
             </div>
         )
